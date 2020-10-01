d7580355d894c5b343d34f4f96fdd40df73f28f1
Wesley Santana <wesleyfsantana@gmail.com>
2020-10-01 20:12:01 -0300

    New optimized version

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,39 @@
-import { json } from 'body-parser';
 import express from 'express';
 import { IntervalsMerge } from './intervals-merge';
 
 
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => {
+const input = [[1, 3], [5, 8], [4, 10], [20, 25]];
 
-  // Input object
-  let input = [[1, 3], [5, 8], [4, 10], [20, 25]];
+app.get('/v1', (req, res) => {
+
+  // Input object  
   console.log(input);
-  
+  console.time('V1 Execution time');
   // Output object
   let output = (new IntervalsMerge()).merge(input);
+  console.timeEnd('V1 Execution time');
   console.log(output);
 
   // Output JSON
   let jsonResult = { input: JSON.stringify(input), output: JSON.stringify(output) };
+  // Response
+  res.send(jsonResult);
+});
 
+app.get('/v2', (req, res) => {
+
+  // Input object
+  console.log(input);
+  console.time('V2 Execution time');
+  // Output object
+  let output = (new IntervalsMerge()).merge(input);
+  console.timeEnd('V2 Execution time');
+  console.log(output);
+
+  // Output JSON
+  let jsonResult = { input: JSON.stringify(input), output: JSON.stringify(output) };
   // Response
   res.send(jsonResult);
 });
--- src/intervals-merge.ts
+++ src/intervals-merge.ts
@@ -2,19 +2,30 @@ import { Interval } from "./interval";
 
 export class IntervalsMerge {
 
+    private isIntersesction(a, b) {
+        return (
+            (a[0] >= b[0] && a[0] <= b[1]) // Pair 'a' init is inside pair 'b'
+            || (a[1] >= b[0] && a[1] <= b[1]) // Pair 'a' end is inside pair 'b'
+            || (b[1] >= a[0] && b[1] <= a[1]) // Pair 'b' init is inside pair 'a'
+            || (b[1] >= a[0] && b[1] <= a[1]) // Pair 'b' end is inside pair 'a'
+        );
+    }
+
     private filterIntersections(interval: Interval, intervals: Interval[]) {
 
         return intervals.filter(i =>
-            ((interval.start >= i.start) && (interval.start <= i.end))
-            || ((interval.end >= i.start) && (interval.end <= i.end))
-            || ((interval.start <= i.start) && (interval.end >= i.end)));
+            ((interval.start >= i.start) && (interval.start <= i.end)) // Pair 'a' init is inside pair 'b'
+            || ((interval.end >= i.start) && (interval.end <= i.end)) // Pair 'a' end is inside pair 'b'
+            || ((i.start >= interval.start) && (i.start <= interval.end)) // Pair 'b' init is inside pair 'a'
+            || ((i.end >= interval.start) && (i.end <= interval.end)) // Pair 'b' end is inside pair 'a'
+        );
     }
 
     public merge(objInput) {
 
         // Mapping to interval list
         let intervalsInput: Interval[] = objInput.map(i => new Interval(i[0], i[1]));
-        
+
         // The result list
         let mergedIntervals: Interval[] = [];
 
@@ -54,5 +65,33 @@ export class IntervalsMerge {
 
         // Mapping and returning result
         return mergedIntervals.map(i => [i.start, i.end]);
+    }
+
+    public mergeV2(objInput) {
+         return this.mergePairs(objInput.pop(), objInput);
+    }
+
+    private mergePairs(pair, input) {
+        let nextInput = []; // List for not merged
+        let p = input.pop(); // Next pair to compare
+        while (p) {
+            if (this.isIntersesction(pair, p)) {
+                // New merged pair with min init and max end
+                pair = [Math.min(pair[0], p[0]), Math.max(pair[1], p[1])];
+            } else {
+                // Store for next merge() call
+                nextInput.push(p);
+            }
+            p = input.pop(); // Next pair to compare
+        }
+        let res = [pair]; // The result with the verified pair
+        // Calling recursively if we have elements not merged
+        if (nextInput.length > 0) {
+            let p = nextInput.pop(); // Next pair to compare
+            res = res.concat(this.mergePairs(p, nextInput)); // Recursively if we've not merged elements
+        }
+
+        // The result
+        return res;
     }
 }
